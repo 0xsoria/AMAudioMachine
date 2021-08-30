ccc404ea2df15b0d834eab3c011c0f3c991ba3d6
Gabriel Sória <gabrielss.rock@me.com>
2021-08-30 17:13:23 -0300

    Initial Commit

---
+++ AMAudioMachine/AMAudioMachine.h
@@ -0,0 +1,18 @@
+//
+//  AMAudioMachine.h
+//  AMAudioMachine
+//
+//  Created by Gabriel Soria Souza on 30/08/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AMAudioMachine.
+FOUNDATION_EXPORT double AMAudioMachineVersionNumber;
+
+//! Project version string for AMAudioMachine.
+FOUNDATION_EXPORT const unsigned char AMAudioMachineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AMAudioMachine/PublicHeader.h>
+
+
